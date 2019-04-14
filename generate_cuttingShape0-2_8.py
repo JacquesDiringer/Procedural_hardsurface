2d589c20d48a199f2e13ae4e9d3e0af5a8f4c748
JacquesDiringer <jacques.diringer@gmail.com>
2019-04-14 14:31:53 +0100

    Clean handling of random seeds + symetry parameter

--- generate_cuttingShape0-2_8.py
+++ generate_cuttingShape0-2_8.py
@@ -1,10 +1,12 @@
 import bpy
 import bmesh
+
 import mathutils
 import random
+from datetime import datetime
 
 # Probabilities.
-randomSeed = 0 # Seed for randomization, different everytime when set to negative.
+randomSeed = -1 # Seed for randomization, different everytime when set to negative.
 rectangleProbability = 0.5 # Probability to pop a rectangle rather than a sphere.
 poppingNewEdge = 1.0 # Probability for each edge to enter a recursive cycle if the recursivity limit has not been hit.
 notchType45 = 0.5 # Probability to pop a 45 degrees notch.
@@ -22,14 +24,19 @@ minmumCircleEdges = 3 # Minimum possible edges in a circle.
 maximumCircleEdges = 6 # Maximum possible edges in a circle.
 
 # Global settings.
-recursivity = 0 # Recursivity limit.
-squareRadius = 10 # Drives the number of shapes generated.
-
+recursivity = 1 # Recursivity limit.
+squareRadius = 5 # Drives the number of shapes generated.
+symetry = False # When True, all edges will have the same details generated, even recursively.
 
 
 # ___________    ->    _____       _____
 #                           |_____|
-def edgeToNotch90(originalBmesh, edge, relativeWidth, relativeDepth, outer):
+
+def edgeToNotch90(seed, originalBmesh, edge, relativeWidth, relativeDepth, outer):
+    
+    # Initialize the random seed, this is important in order to generate exactly the same content for a given seed.
+    random.seed(seed)
+    
     # Remember the original 2 vertices.
     vertA = edge.verts[0]
     vertB = edge.verts[1]
@@ -76,7 +83,11 @@ def edgeToNotch90(originalBmesh, edge, relativeWidth, relativeDepth, outer):
 
 # ___________    ->    _____       _____
 #                           \_____/
-def edgeToNotch45(originalBmesh, edge, relativeWidth, relativeDepth, outer):
+
+def edgeToNotch45(seed, originalBmesh, edge, relativeWidth, relativeDepth, outer):
+    
+    # Initialize the random seed, this is important in order to generate exactly the same content for a given seed.
+    random.seed(seed)
     
     originalBmesh.edges.ensure_lookup_table()
     
@@ -124,7 +135,11 @@ def edgeToNotch45(originalBmesh, edge, relativeWidth, relativeDepth, outer):
     return newEdges
     
     
-def genericEdgeTransformation(originalBmesh, edgeToTransform, recursionDepth):
+def genericEdgeTransformation(seed, originalBmesh, edgeToTransform, recursionDepth):
+    
+    # Initialize the random seed, this is important in order to generate exactly the same content for a given seed.
+    random.seed(seed)
+    
     # Decide if there is going to be a recursion pass for this edge.
     if(random.uniform(0, 1) < poppingNewEdge):
         
@@ -135,21 +150,30 @@ def genericEdgeTransformation(originalBmesh, edgeToTransform, recursionDepth):
         
         # Decide what the next recursive function is.
         if(random.uniform(0, 1) < notchType45):
-            returnedEdges = edgeToNotch45(originalBmesh, edgeToTransform, currentWidth, currentDepth, outer)
+            returnedEdges = edgeToNotch45(seed, originalBmesh, edgeToTransform, currentWidth, currentDepth, outer)
         else:
-            returnedEdges = edgeToNotch90(originalBmesh, edgeToTransform, currentWidth, currentDepth, outer)
+            returnedEdges = edgeToNotch90(seed, originalBmesh, edgeToTransform, currentWidth, currentDepth, outer)
         
         # Recursion.
         if recursionDepth > 0:
             for currentEdge in returnedEdges:
-                genericEdgeTransformation(originalBmesh, currentEdge, recursionDepth - 1)
+                if symetry:
+                    # When symetry is enabled, take the same seed for every edge.
+                    futureSeed = seed
+                else:
+                    # When symetry is disabled, randomize the seed for every edge.
+                    futureSeed = random.randint(0, 1000000)
+            
+                genericEdgeTransformation(futureSeed, originalBmesh, currentEdge, recursionDepth - 1)
         
         originalBmesh.edges.ensure_lookup_table()
         
 
 # Loops trough edges to add detail recursively.
 # The shape to change must be selected before entering the function.
-def genericShapeTransformation(recursionDepth):
+def genericShapeTransformation(seed, recursionDepth):
+    # Initialize the random seed, this is important in order to generate exactly the same content for a given seed.
+    random.seed(seed)
     
     # Enter edit mode.
     bpy.ops.object.mode_set(mode = 'EDIT')
@@ -175,18 +199,19 @@ def genericShapeTransformation(recursionDepth):
     print("Initial edges = " + str(len(bm.edges)))
         
     for currentEdge in range(0, len(bm.edges)):
-        genericEdgeTransformation(bm, bm.edges[currentEdge], recursionDepth)
+        if symetry:
+            # When symetry is enabled, take the same seed for every edge.
+            futureSeed = seed
+        else:
+            # When symetry is disabled, randomize the seed for every edge.
+            futureSeed = random.randint(0, 1000000)
+            
+        genericEdgeTransformation(futureSeed, bm, bm.edges[currentEdge], recursionDepth)
             
         
     print("Final edges = " + str(len(bm.edges)))
 
     bm.edges.ensure_lookup_table()
-
-    # Select random vertices.
-    # Set the selection mpde to edges.
-    bpy.ops.mesh.select_mode(type="VERT")
-    bpy.ops.mesh.select_all(action='DESELECT')
-    bpy.ops.mesh.select_random()
 
 
     # Show the updates in the viewport
@@ -200,7 +225,7 @@ def genericShapeTransformation(recursionDepth):
     
         
 # Generates a circle shape of 6 edges with recursive details.
-def generateCircleCuttingShape(position, dimension, edgeCount, recursionDepth):
+def generateCircleCuttingShape(seed, position, dimension, edgeCount, recursionDepth):
     
     # Generate a plane.
     bpy.ops.mesh.primitive_circle_add(radius=0.4, vertices=edgeCount, location=(position))
@@ -211,13 +236,12 @@ def generateCircleCuttingShape(position, dimension, edgeCount, recursionDepth):
     bpy.ops.transform.resize(value=(dimension[0], dimension[1], 1.0), orient_type='GLOBAL')
     bpy.ops.object.mode_set(mode = 'OBJECT')
 
-
     # Generic function to add details on edges.
-    genericShapeTransformation(recursionDepth)    
+    genericShapeTransformation(seed, recursionDepth)    
     
     
 # Generates a rectangular shape with recursive details.
-def generateRectangleCuttingShape(position, dimension, recursionDepth):
+def generateRectangleCuttingShape(seed, position, dimension, recursionDepth):
 
     # Generate a plane.
     bpy.ops.mesh.primitive_plane_add(size=0.8, view_align=False, enter_editmode=False, location=(position))
@@ -229,32 +253,43 @@ def generateRectangleCuttingShape(position, dimension, recursionDepth):
     bpy.ops.object.mode_set(mode = 'OBJECT')
     
     # Generic function to add details on edges.
-    genericShapeTransformation(recursionDepth)
+    genericShapeTransformation(seed, recursionDepth)
     
     
-    
-    # Main
+def generateGenericShape(seed, position):
+    if random.uniform(0,1) < rectangleProbability :
+        # Generate a rectangle of random dimension.
+        downscaleValue = random.uniform(maximumRatioDifference, 1.0)
+        # Apply downscale on either x or y.
+        if random.uniform(0,1) < verticalProbability :
+            generateRectangleCuttingShape(seed=seed, position=position, dimension=((downscaleValue, 1)), recursionDepth=recursivity)
+        else:
+            generateRectangleCuttingShape(seed=seed, position=position, dimension=((1, downscaleValue)), recursionDepth=recursivity)
+            
+    else:
+        # Generate a circle of random vertices.
+        verticesCount = random.uniform(minmumCircleEdges, maximumCircleEdges)
+        generateCircleCuttingShape(seed=seed, position=position, dimension=((1.0, 1.0)), edgeCount=verticesCount, recursionDepth=recursivity)
+
+
+
+
+
+
+
+# Main
 
 print("starting main")
 
-# Intialize random seed
+# Intialize seed.
 if randomSeed >= 0 :
     random.seed(randomSeed)
+else:
+    # Take the current time to randomize the seed when we want it different each time.
+    random.seed(datetime.now())
 
-#generateSquareCuttingShape(position=(0, 0, 0), recursionDepth=recursivity)
+#generateGenericShape(seed=randomSeed, position=(0, 0, 0))
 
 for xCoords in range(-squareRadius, squareRadius):
     for yCoords in range(-squareRadius, squareRadius):
-        if random.uniform(0,1) < rectangleProbability :
-            # Generate a rectangle of random dimension.
-            downscaleValue = random.uniform(maximumRatioDifference, 1.0)
-            # Apply downscale on either x or y.
-            if random.uniform(0,1) < verticalProbability :
-                generateRectangleCuttingShape(position=(xCoords, yCoords, 0), dimension=((downscaleValue, 1)), recursionDepth=recursivity)
-            else:
-                generateRectangleCuttingShape(position=(xCoords, yCoords, 0), dimension=((1, downscaleValue)), recursionDepth=recursivity)
-                
-        else:
-            # Generate a circle of random vertices.
-            verticesCount = random.uniform(minmumCircleEdges, maximumCircleEdges)
-            generateCircleCuttingShape(position=(xCoords, yCoords, 0), dimension=((1.0, 1.0)), edgeCount=verticesCount, recursionDepth=recursivity)
+        generateGenericShape(seed=random.randint(0, 1000000), position=(xCoords, yCoords, 0))
