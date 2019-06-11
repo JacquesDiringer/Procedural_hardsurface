a2233ebd62b8260c421ff627dab21ee29279c068
JacquesDiringer <jacques.diringer@gmail.com>
2019-06-11 20:43:00 +0100

    fixed clean face cutting for recursive cutting, works only on the first recursion for now

--- cut_surface0_2_8.py
+++ cut_surface0_2_8.py
@@ -33,6 +33,8 @@ cleanFaceMargin = 0.9
 
 def knifeProject(surfaceToCut, surfaceCuter):
     
+    bpy.ops.object.mode_set(mode = 'OBJECT')
+    
     # Arrange selection for knife cutting operation.
     bpy.context.view_layer.objects.active = surfaceToCut
     surfaceCuter.select_set(True)
@@ -135,7 +137,25 @@ def cutPlate(seed, objectToCut, faceToCut, cuttingShape):
 
 # The responsibility of this function is to generate the cutting shape and place it correctly for the cutPlate function.
 # It should then cut the inner rectangle to enable recursivity.
-def genericCutPlate(seed, objectToCut, faceToCut):
+def genericCutPlate(seed, objectToCut, faceTuple):
+    
+    print()
+    print("new cut")
+    
+    if objectToCut == None:
+        print("Tried to cut a None object")
+        return
+        
+    if faceTuple == None:
+        print("Tried to cut a None tuple")
+        return
+    
+    # Find the face in the object.
+    faceToCut = objectToCut.data.polygons[faceTuple[0]]
+    
+    if faceToCut == None:
+        print("Tried to cut a None face")
+        return
     
     # Initialize the random seed, this is important in order to generate exactly the same content for a given seed.
     random.seed(seed)
@@ -147,52 +167,76 @@ def genericCutPlate(seed, objectToCut, faceToCut):
     faceWidth   = rectDimension[1] - rectDimension[0]
     faceHeight  = rectDimension[3] - rectDimension[2]
     facePosition = ((rectDimension[1] + rectDimension[0]) * 0.5, (rectDimension[3] + rectDimension[2]) * 0.5, 0)
+    
+    print("faceWidth = " + str(faceWidth))
+    print("faceHeight = " + str(faceHeight))
 
     # Generate a cutting shape
-    cuttingShape, edgesDepth = generateRectangleCuttingShape(seed=random.randint(0, 100000), position=facePosition, dimension=(1,1), recursionDepth=0)
+    cuttingShapeDimension = (faceWidth * 0.5, faceHeight * 0.5)
+    cuttingShape, edgesDepth = generateRectangleCuttingShape(seed=random.randint(0, 100000), position=facePosition, dimension=cuttingShapeDimension, recursionDepth=0)
+    
+    print("edgesDepth before fix = " + str(edgesDepth))
+    
+    # Fix edges depth.
+    edgesDepth[0] = edgesDepth[0] * cuttingShapeDimension[0]
+    edgesDepth[1] = edgesDepth[1] * cuttingShapeDimension[1]
+    edgesDepth[2] = edgesDepth[2] * cuttingShapeDimension[0]
+    edgesDepth[3] = edgesDepth[3] * cuttingShapeDimension[1]
     
     # Compute the inner bounds of the cutting shape.
-    print("edgesDepth = " + str(edgesDepth))
+    print("edgesDepth after fix = " + str(edgesDepth))
 
-    cuttingShapeDimension = (1,1)                               
-
-    cuttingShapeOuterBounds =  [cuttingShapeDimension[0]/2 + max(0, edgesDepth[0]), # left
-                                cuttingShapeDimension[1]/2 + max(0, edgesDepth[1]), # bottom
+    cuttingShapeOuterBounds =  [-cuttingShapeDimension[0]/2 - max(0, edgesDepth[0]), # left
+                                -cuttingShapeDimension[1]/2 - max(0, edgesDepth[1]), # bottom
                                 cuttingShapeDimension[0]/2 + max(0, edgesDepth[2]), # right
                                 cuttingShapeDimension[1]/2 + max(0, edgesDepth[3])] # up
 
-    cuttingShapeOuterBoundsDimension = (cuttingShapeOuterBounds[0] + cuttingShapeOuterBounds[2], # width
-                                        cuttingShapeOuterBounds[1] + cuttingShapeOuterBounds[3]) # height
-                                        
-    cuttingShapeOuterBoundsOffset = (cuttingShapeOuterBounds[2] - cuttingShapeOuterBounds[0], # width
+    cuttingShapeOuterBoundsDimension = (cuttingShapeOuterBounds[2] - cuttingShapeOuterBounds[0], # width
                                         cuttingShapeOuterBounds[3] - cuttingShapeOuterBounds[1]) # height
+                                        
+    cuttingShapeOuterBoundsOffset = ((  cuttingShapeOuterBounds[2] + cuttingShapeOuterBounds[0]) * 0.5, # width
+                                    (   cuttingShapeOuterBounds[3] + cuttingShapeOuterBounds[1]) * 0.5) # height
+                                        
+    print("cuttingShapeOuterBounds = " + str(cuttingShapeOuterBounds))
+    print("cuttingShapeOuterBoundsDimension = " + str(cuttingShapeOuterBoundsDimension))
+    print("cuttingShapeOuterBoundsOffset = " + str(cuttingShapeOuterBoundsOffset))
+    
+    
+    # Translation.
+    cuttingShapeTranslation = (-cuttingShapeOuterBoundsOffset[0], -cuttingShapeOuterBoundsOffset[1], 0)
+    bpy.ops.transform.translate(value=cuttingShapeTranslation, orient_type='GLOBAL', orient_matrix=((1, 0, 0), (0, 1, 0), (0, 0, 1)), orient_matrix_type='GLOBAL')
+    
+    print("cuttingShapeTranslation = " + str(cuttingShapeTranslation))
     
     
     # The cutting shape should be re-scaled and translated to fit the face to cut.
     # Scale it uniformly however.
     cuttingShapeScaleFactor = cuttingShapeMargin * min(faceWidth / cuttingShapeOuterBoundsDimension[0], faceHeight / cuttingShapeOuterBoundsDimension[1])
+    # Correct center for the resize.
+    bpy.context.scene.tool_settings.transform_pivot_point = 'MEDIAN_POINT'
+    # Resize operation.
     bpy.ops.transform.resize(value=(cuttingShapeScaleFactor, cuttingShapeScaleFactor, 1), orient_type='GLOBAL', orient_matrix=((1, 0, 0), (0, 1, 0), (0, 0, 1)), orient_matrix_type='GLOBAL')                                   
-    # Translation.
-    cuttingShapeTranslation = (-cuttingShapeOuterBoundsOffset[0] * 0.5 * cuttingShapeScaleFactor, -cuttingShapeOuterBoundsOffset[1] * 0.5 * cuttingShapeScaleFactor, 0)
-    bpy.ops.transform.translate(value=cuttingShapeTranslation, orient_type='GLOBAL', orient_matrix=((1, 0, 0), (0, 1, 0), (0, 0, 1)), orient_matrix_type='GLOBAL')
+    
+    print("cuttingShapeScaleFactor = " + str(cuttingShapeScaleFactor))
 
     
     # After this rescale, the inner bounds can be calculated
-    cuttingShapeInnerBounds =  [(cuttingShapeDimension[0]/2 + min(0, edgesDepth[0])) * cuttingShapeScaleFactor, # left
-                                (cuttingShapeDimension[1]/2 + min(0, edgesDepth[1])) * cuttingShapeScaleFactor, # bottom
-                                (cuttingShapeDimension[0]/2 + min(0, edgesDepth[2])) * cuttingShapeScaleFactor, # right
-                                (cuttingShapeDimension[1]/2 + min(0, edgesDepth[3])) * cuttingShapeScaleFactor] # up
+    cuttingShapeInnerBounds =  [(-cuttingShapeDimension[0]/2 - min(0, edgesDepth[0])) * cuttingShapeScaleFactor + cuttingShapeTranslation[0], # left
+                                (-cuttingShapeDimension[1]/2 - min(0, edgesDepth[1])) * cuttingShapeScaleFactor + cuttingShapeTranslation[1] , # bottom
+                                ( cuttingShapeDimension[0]/2 + min(0, edgesDepth[2])) * cuttingShapeScaleFactor + cuttingShapeTranslation[0], # right
+                                ( cuttingShapeDimension[1]/2 + min(0, edgesDepth[3])) * cuttingShapeScaleFactor + cuttingShapeTranslation[1]] # up
 
-    cuttingShapeInnerBoundsDimension = (cuttingShapeInnerBounds[0] + cuttingShapeInnerBounds[2], # width
-                                        cuttingShapeInnerBounds[1] + cuttingShapeInnerBounds[3]) # height
+    cuttingShapeInnerBoundsDimension = (cuttingShapeInnerBounds[2] - cuttingShapeInnerBounds[0], # width
+                                        cuttingShapeInnerBounds[3] - cuttingShapeInnerBounds[1]) # height
                                         
-    cuttingShapeInnerBoundsOffset = (   cuttingShapeInnerBounds[2] - cuttingShapeInnerBounds[0] - cuttingShapeTranslation[0], # width
-                                        cuttingShapeInnerBounds[3] - cuttingShapeInnerBounds[1] - cuttingShapeTranslation[1]) # height
-
+    cuttingShapeInnerBoundsOffset = (   (cuttingShapeInnerBounds[2] + cuttingShapeInnerBounds[0]) * 0.5, # width
+                                        (cuttingShapeInnerBounds[3] + cuttingShapeInnerBounds[1]) * 0.5) # height
+                                        
+                                        
     
     print("cuttingShapeInnerBounds = " + str(cuttingShapeInnerBounds))
-    
-    print("dimensions = " + str(cuttingShapeInnerBoundsDimension))
+    print("cuttingShapeInnerBoundsDimension = " + str(cuttingShapeInnerBoundsDimension))
+    print("cuttingShapeInnerBoundsOffset = " + str(cuttingShapeInnerBoundsOffset))
     
     
     # Cut the plate with the tech-ish shape.
@@ -203,8 +247,16 @@ def genericCutPlate(seed, objectToCut, faceToCut):
     ## Cut the surface again to have a clean surface to work with for recursivity.
     # Generate a plane cutting shape.
     bpy.ops.mesh.primitive_plane_add(size=1, view_align=False, enter_editmode=True, location=(facePosition))
-    bpy.ops.transform.resize(value=(cuttingShapeInnerBoundsDimension[0] * cleanFaceMargin, cuttingShapeInnerBoundsDimension[1] * cleanFaceMargin, 1), orient_type='GLOBAL', orient_matrix=((1, 0, 0), (0, 1, 0), (0, 0, 1)), orient_matrix_type='GLOBAL')
-    bpy.ops.transform.translate(value=(-cuttingShapeInnerBoundsOffset[0] * 0.5, -cuttingShapeInnerBoundsOffset[1] * 0.5, 0), orient_type='GLOBAL', orient_matrix=((1, 0, 0), (0, 1, 0), (0, 0, 1)), orient_matrix_type='GLOBAL')
+    # Re-scale to fit the plane in the inner bounds.
+    cleanFaceScale = (cuttingShapeInnerBoundsDimension[0] * cleanFaceMargin, cuttingShapeInnerBoundsDimension[1] * cleanFaceMargin, 1)
+    bpy.ops.transform.resize(value=cleanFaceScale, orient_type='GLOBAL', orient_matrix=((1, 0, 0), (0, 1, 0), (0, 0, 1)), orient_matrix_type='GLOBAL')
+    # Translate to fit the plane in the inner bounds.
+    cleanFaceTranslation = (cuttingShapeInnerBoundsOffset[0], cuttingShapeInnerBoundsOffset[1], 0)
+#    cleanFaceTranslation = (0.06072782036990082, 0.009383141258685945, 0)
+    bpy.ops.transform.translate(value=cleanFaceTranslation, orient_type='GLOBAL', orient_matrix=((1, 0, 0), (0, 1, 0), (0, 0, 1)), orient_matrix_type='GLOBAL')
+    
+    print("cleanFaceScale = " + str(cleanFaceScale))
+    print("cleanFaceTranslation = " + str(cleanFaceTranslation))
 
     # Go back to object mode.
     bpy.ops.object.mode_set(mode = 'OBJECT')
@@ -220,15 +272,30 @@ def genericCutPlate(seed, objectToCut, faceToCut):
     bpy.data.objects.remove(cuttingShape)
     bpy.data.meshes.remove(dataToRemove)
     
-    return resultingFace
+    if not resultingFace == None:
+        resultingFaceTuple = buildFaceTuple(objectToCut, resultingFace.index)
+        return resultingFaceTuple
 
 
 # Test function
 if __name__ == "__main__":
+    
+    # Delete everything in the scene.
+    bpy.ops.object.mode_set(mode = 'OBJECT')
+    bpy.ops.object.select_all(action='SELECT')
+    bpy.ops.object.delete(use_global=False, confirm=False)
+
+    # Create a new plane.
+    bpy.ops.mesh.primitive_plane_add(view_align=False, enter_editmode=False, location=(0, 0, 0))
+    
     # Keep track of the selected object.
     originalySelectedObject = bpy.context.active_object
 
     # Cut a plate in the selected object.
-    resultingFace = genericCutPlate(2, originalySelectedObject, originalySelectedObject.data.polygons[0])
-    resultingFace = genericCutPlate(2, originalySelectedObject, resultingFace)
+    resultingFaceTuple = genericCutPlate(0, originalySelectedObject, buildFaceTuple(originalySelectedObject, originalySelectedObject.data.polygons[0].index))
+#    resultingFaceTuple = genericCutPlate(0, originalySelectedObject, resultingFaceTuple)
+#    resultingFaceTuple = genericCutPlate(0, originalySelectedObject, resultingFaceTuple)
+#    resultingFaceTuple = genericCutPlate(0, originalySelectedObject, resultingFaceTuple)
+#    resultingFaceTuple = genericCutPlate(0, originalySelectedObject, resultingFaceTuple)
+    bpy.ops.object.mode_set(mode = 'EDIT')
 #datetime.now()
